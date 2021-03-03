a3d7005d00747db741da97df8dafe5957b8ab67c
Marc Miller <millermj@dgs1.sdt>
2021-03-03 15:55:26 -0500

    end of day/working with lifecycle methods/state handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = { lat: null, errorMessage: ''};
+
+    window.navigator.geolocation.getCurrentPosition(
+        position => {this.setState({lat: position.coords.latitude})},
+        err => {this.setState({errorMessage: err.message})}
+    );
+  }
+
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('My component was updated and rerendered');
+  }
+
+
+  render(){
+        if(this.state.errorMessage && !this.state.latitude){
+          return(
+              <div>Error: {this.state.errorMessage}</div>
+          );
+        }
+
+        if(!this.state.errorMessage && this.state.lat){
+          return(
+              <div>Latitude: {this.state.lat}</div>
+          );
+        }
+        return(
+          <div>Loading...</div>
+        );
+
+
+      }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
