3f55a490a26a74b09b4e0457e23d59f2059b4b15
Marc Miller <millermj@dgs1.sdt>
2021-03-04 11:19:59 -0500

    completed seasons lesson/state/lifecycle methods/coditional rendering

--- src/index.js
+++ src/index.js
@@ -1,46 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./components/SeasonDisplay";
+import Spinner from "./components/Spinner";
 
 
 class App extends React.Component{
-  constructor(props){
-    super(props)
+  state = {lat: null, errorMessage: ''}
 
-    this.state = { lat: null, errorMessage: ''};
-
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
     window.navigator.geolocation.getCurrentPosition(
         position => {this.setState({lat: position.coords.latitude})},
         err => {this.setState({errorMessage: err.message})}
     );
   }
 
-  componentDidMount() {
-    console.log('My component was rendered to the screen');
-  }
-
   componentDidUpdate(prevProps, prevState, snapshot) {
     console.log('My component was updated and rerendered');
   }
 
+  renderContent(){
+    if(this.state.errorMessage && !this.state.latitude){
+      return(
+          <div>Error: {this.state.errorMessage}</div>
+      );
+    }
+
+    if(!this.state.errorMessage && this.state.lat){
+      return(
+          <div>
+            <SeasonDisplay
+                lat={this.state.lat}
+            />
+          </div>
+      );
+    }
+    return(
+        <div>
+          <Spinner
+              text="Waiting for user location permission..."
+          />
+        </div>
+    );
+  }
+
 
   render(){
-        if(this.state.errorMessage && !this.state.latitude){
-          return(
-              <div>Error: {this.state.errorMessage}</div>
-          );
-        }
-
-        if(!this.state.errorMessage && this.state.lat){
-          return(
-              <div>Latitude: {this.state.lat}</div>
-          );
-        }
-        return(
-          <div>Loading...</div>
-        );
-
-
-      }
+       return (
+        <div className="border red">{this.renderContent()}</div>
+       );
+      };
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
